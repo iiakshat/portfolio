89d85c0a48bcb47dc9f506ff7252063c625dfb1e
iiakshat <jakshat569@gmail.com>
2024-11-22 12:59:47 +0530

    Update Home
    - Removed Unnecessary imports
    - Added Animation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,14 @@
-window.addEventListener('load', () => {
-    document.body.classList.add('loaded');
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("loaded");
+  const homeContainer = document.querySelector(".homecontainer");
+  if (homeContainer) {
+    homeContainer.classList.add("loaded");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const aboutSection = document.querySelector(".about-section");
+  if (aboutSection) {
+    aboutSection.classList.add("loaded");
+  }
+});
